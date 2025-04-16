854c1d22ddbc71d66f20320a8b841d97b26a86f2
Ravann <revann.gitlab@gmail.com>
2025-04-16 13:30:05 +0400

    practice portal

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -4,14 +4,16 @@ import Details from "@/components/ui/home/details/Details";
 import WhyChooseUs from "@/components/ui/home/why-choose-us/WhyChooseUs";
 import PopularCourses from "@/components/ui/home/popular-courses/PopularCourses";
 import Instructors from "@/components/ui/home/instructors/Instructors";
+import PracticePortal from "@/components/ui/home/practice-portal/PracticePortal";
 
 const Home = () => {
   return (
     <>
       <Details />
       <WhyChooseUs />
-      <PopularCourses/>
-      <Instructors/>
+      <PopularCourses />
+      <Instructors />
+      <PracticePortal />
     </>
   );
 };
