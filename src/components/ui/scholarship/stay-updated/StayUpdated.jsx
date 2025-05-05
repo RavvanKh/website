6928d12bcc1231987aca3303ec525ff9f856c82a
Ravann <revann.gitlab@gmail.com>
2025-05-05 13:52:35 +0400

    stay updated

---
+++ src/components/ui/scholarship/stay-updated/StayUpdated.jsx
@@ -0,0 +1,35 @@
+"use client";
+import { useI18n } from "@/locales/client";
+
+import styles from "./stay-updated.module.css";
+import Image from "next/image";
+
+const StayUpdated = () => {
+  const t = useI18n();
+  return (
+    <section className={styles.stayUpdated}>
+      <div className={styles.stayUpdatedContainer}>
+        <div className={styles.stayUpdatedLeft}>
+          <div className={styles.stayUpdatedIcon}>
+            <Image
+              src="/icons/astronaut.svg"
+              height={121}
+              width={117}
+              alt="Astronaut"
+              loading="lazy"
+            />
+          </div>
+          <div className={styles.stayUpdatedContent}>
+            <div>{t("stayUpdatedTitle")}</div>
+            <p>{t("stayUpdatedDescription")}</p>
+          </div>
+        </div>
+        <div className={styles.stayUpdatedRight}>
+          <button>{t("getStartNow")}</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StayUpdated;
