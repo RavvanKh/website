d8c781b457859c1e6bf3214a116711f87635ba10
Ravann <revann.gitlab@gmail.com>
2025-05-07 12:23:58 +0400

    add location

--- src/components/shared/course-application/CourseApplication.jsx
+++ src/components/shared/course-application/CourseApplication.jsx
@@ -8,13 +8,16 @@ import * as yup from "yup";
 
 import { useI18n } from "@/locales/client";
 
-import {contacts } from "@/data/contact";
+import { contacts } from "@/data/contact";
 
 import ContactSocials from "@/components/shared/contact-socials/ContactSocials";
+
+import GetInTouch from "../get-in-touch/GetInTouch";
 
 import { createCourseApplication } from "@/lib/utils/api/courseApplication";
 
 import styles from "./course-application.module.css";
+
 
 const CourseApplication = ({ courses }) => {
   const t = useI18n();
@@ -53,43 +56,7 @@ const CourseApplication = ({ courses }) => {
   return (
     <section className={styles.courseApplication}>
       <div className={styles.courseApplicationContainer}>
-        <div className={styles.courseApplicationLeft}>
-          <div className={styles.courseApplicationLeftTop}>
-            <div className={styles.courseApplicationLeftTopTitle}>
-              {t("getInTouch")}
-            </div>
-            <p className={styles.courseApplicationLeftDescription}>
-              {t("contactUsDirectlyForQuickInteraction")}
-            </p>
-            {contacts.map((contact) => (
-              <div
-                className={styles.courseApplicationContact}
-                key={contact.key}
-              >
-                <div className={styles.courseApplicationContactIcon}>
-                  <Image src={contact.icon2} height={18} width={24} alt="Icon" />
-                </div>
-                <div>
-                  <div className={styles.contactForCourseApplicationContactKey}>
-                    {t(contact.key)}
-                  </div>
-                  <Link
-                    href={contact.url}
-                    className={styles.contactForCourseApplicationContactText}
-                  >
-                    {t(contact.text)}
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className={styles.courseApplicationLeftBottom}>
-            <div className={styles.courseApplicationLeftBottomTitle}>
-              {t("socialMedia")}
-            </div>
-            <ContactSocials />
-          </div>
-        </div>
+        <GetInTouch />
 
         <div className={styles.courseApplicationRight}>
           <div className={styles.courseApplicationRightTitle}>
@@ -103,7 +70,7 @@ const CourseApplication = ({ courses }) => {
             onSubmit={handleSubmit(onSubmit)}
           >
             <div className={styles.courseApplicationFormInputGroup}>
-            <label htmlFor="course">{t("course")}</label>
+              <label htmlFor="course">{t("course")}</label>
               <select
                 {...register("courseId", { required: true })}
                 defaultValue=""
