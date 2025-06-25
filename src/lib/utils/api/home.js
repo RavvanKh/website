254c589a212f77357b6521c07017f06e7996a87b
Ravann <revann.gitlab@gmail.com>
2025-06-25 10:55:58 +0400

    remove data from console

--- src/lib/utils/api/home.js
+++ src/lib/utils/api/home.js
@@ -6,7 +6,6 @@ import { cache } from "react";
 export const getHomeData = cache(async () => {
   try {
     const res = await customAxios.get(`/v1/home`);
-    console.log(res.data);
     return res.data;
   } catch (err) {
     throw new Error(err);
