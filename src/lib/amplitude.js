87e3950c1d38c338ea1bb8bef37f6b75a522391f
Ravann <revann.gitlab@gmail.com>
2025-09-08 10:59:53 +0400

    session id

--- src/lib/amplitude.js
+++ src/lib/amplitude.js
@@ -1,22 +1,27 @@
 import * as amplitude from "@amplitude/analytics-browser";
-import {
-  sessionReplayPlugin,
-} from "@amplitude/plugin-session-replay-browser";
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
+
+let replayPlugin = null;
 
 export const initAmplitude = () => {
   const apiKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
 
-  console.log(apiKey);
   amplitude.init(apiKey, {
     defaultTracking: true,
-    serverZone: "EU",
+    serverZone: "US",     
+    fetchRemoteConfig: true,
+    autocapture: true,
+    deviceId: window?.crypto?.randomUUID?.(),
   });
 
-  const sessionReplayTracking = sessionReplayPlugin({ sampleRate: 1 });
-
-  amplitude.add(sessionReplayTracking);
+  replayPlugin = sessionReplayPlugin({ sampleRate: 1 });
+  amplitude.add(replayPlugin);
 };
 
-export const logEvent = (eventName, eventProperties) => {
-  amplitude.track(eventName, eventProperties);
+export const logEvent = (eventName, eventProperties = {}) => {
+  const replayId = replayPlugin?.getSessionReplayId?.();
+  amplitude.track(eventName, {
+    ...eventProperties,
+    replay_id: replayId, 
+  });
 };
